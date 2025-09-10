70d5add266c916e8b42b5a0f7c19a86297b47f62
REETIRAJ GAUTAM <166306283+reetiraj123@users.noreply.github.com>
2025-09-10 11:56:06 +0530

    Create admin.py

---
+++ stats/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Local Imports
+from stats.models import TrackLesson
+
+
+class TrackLessonAdmin(admin.ModelAdmin):
+    search_fields = ['user__first_name', 'user__last_name', 'user__username',
+                     'course__name', 'lesson__name']
+    readonly_fields = ["course", "user", "lesson"]
+
+
+admin.site.register(TrackLesson, TrackLessonAdmin)
