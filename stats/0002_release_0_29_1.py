b2168321c0f174b646f7ae383d826a1cb60f89f9
REETIRAJ GAUTAM <166306283+reetiraj123@users.noreply.github.com>
2025-09-10 11:52:17 +0530

    Create 0002_release_0_29_1.py

---
+++ stats/0002_release_0_29_1.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('stats', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='lessonlog',
+            name='current_time',
+            field=models.CharField(default='00:00:00', max_length=20),
+        ),
+        migrations.AddField(
+            model_name='tracklesson',
+            name='watched',
+            field=models.BooleanField(default=False),
+        ),
+    ]
